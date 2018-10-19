31600ce20a10271619806f68fb07252d99dffb14
darknessinsideme <hasanu12240998@gmail.com>
2018-10-19 21:10:56 +0300

    MY UPDATE HOMEWORK

---
+++ shell the update one.sh
@@ -0,0 +1,25 @@
+mkdir fristhwk
+cd ~/Desktop/sha/fristhwk
+touch blank.txt 
+touch greeting.txt
+echo "hello hello hello hello hello" > greeting.txt
+
+cp greeting.txt 1.txt
+cp greeting.txt 2.txt 
+cp greeting.txt 3.txt 
+cp greeting.txt 4.txt
+cp greeting.txt 5.txt 
+
+echo "cat" > pets.txt
+echo " dog" >> pets.txt
+echo " hamster" >> pets.txt
+
+echo "cat " > command.txt 
+echo "ls" >> commands.txt 
+echo " pwd" >> commands.txt
+
+cat pets.txt commands.txt |sort | uniq > lovelyCommands.txtl
+
+ 
+
+
